bcffb81809e38004d150e3df4f1ca22b10fcdcb0
hequan <crabapples.cn@gmailcom>
2021-09-09 00:52:04 +0800

    demo file

---
+++ helloworld/click-prod.js
@@ -0,0 +1,13 @@
+// log(selector().text("零售专区").algorithm("BFS").find());
+// var button = selector().text("零售专区").algorithm("BFS").find()
+// button.click()
+toast('3秒后开始点击')
+sleep(3000)
+    click(486,855)
+    sleep(500)
+    scrollDown(3)
+    sleep(500)
+    click(30,726)
+    // sleep(2000)
+
+    // click(435,1772,1065,1892)
